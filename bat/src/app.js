727310e98d4997f44f2cc2e15b79cc74d2d1a32f
brunooomelo <utibmelo@gmail.com>
2018-11-03 14:01:55 -0300

    adicionando paginação

--- bat/src/app.js
+++ bat/src/app.js
@@ -1,41 +1,43 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const { Upload } = require('./config/multer')
-const Bat = require('./models/bat')
-require('./config/database')
-const app = express()
+const express = require("express");
+const { Upload } = require("./config/multer");
+const Bat = require("./models/bat");
+const middle = require("./services/auth");
+require("./config/database");
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(express.json());
 
-app.get('/bat', async (req, res) => {
-  const data = await Bat.find()
-  res.json({ bat: data })
-})
+app.get("/bat", middle, async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+  const data = await Bat.paginate({
+    page: parseInt(page),
+    limit: parseInt(limit)
+  });
+  res.json({ bat: data });
+});
 
-app.get('/bat/:id', async (req, res) => {
-  const { id } = req.params
-  const data = await Bat.findOne({ _id: id })
-  res.json({ user: data })
-})
+app.get("/bat/:id", middle, async (req, res) => {
+  const { id } = req.params;
+  const data = await Bat.findOne({ _id: id });
+  res.json({ user: data });
+});
 
-app.post('/bat', Upload.single('file'), async (req, res) => {
-  let data = new Bat({ ...req.body, file: {...req.file}, star: 0 })
-  res.json({ data: data.save() })
-})
+app.post("/bat", middle, Upload.single("file"), async (req, res) => {
+  let data = new Bat({ ...req.body, file: { ...req.file }, star: 0 });
+  res.json({ data: data.save() });
+});
 
-app.post('/bat/:id/star', async (req, res) => {
-  const { id } = req.params
-  let bat = await Bat.findOne({ _id: id })
-  let data = await Bat.updateOne({ _id: id }, { star: bat.star + 1 })
-  res.json({ data })
-})
+app.post("/bat/:id/star", middle, async (req, res) => {
+  const { id } = req.params;
+  let bat = await Bat.findOne({ _id: id });
+  let data = await Bat.updateOne({ _id: id }, { star: bat.star + 1 });
+  res.json({ data });
+});
 
-app.patch('/bat/:id', async (req, res) => {
-  const { id } = req.params
-  let data = await Bat.updateOne({ _id: id }, req.body)
-  res.json({ data })
-})
+app.patch("/bat/:id", middle, async (req, res) => {
+  const { id } = req.params;
+  let data = await Bat.updateOne({ _id: id }, req.body);
+  res.json({ data });
+});
 
-
-app.listen('3001', () => ( console.log('Server listen 3001') ))
+app.listen("3001", () => console.log("server listering in port 3001"));
