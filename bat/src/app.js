d7c1b0e8486a7ede862938e14b9df9a4387b6b19
brunooomelo <utibmelo@gmail.com>
2018-11-04 14:41:52 -0200

    - configurado os endpoint - ajustado com padrao eslint - try / catch e paginacao

--- bat/src/app.js
+++ bat/src/app.js
@@ -1,53 +1,74 @@
-const express = require("express");
-const { Upload } = require("./config/multer");
-const Bat = require("./models/bat");
-const middle = require("./services/auth");
-require("./config/database");
-const app = express();
+const express = require('express')
+const { Upload } = require('./config/multer')
+const Bat = require('./models/bat')
+const middle = require('./services/auth')
+const path = require('path')
+require('./config/database')
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.get("/bat", middle, async (req, res) => {
-  try {
-    const { page = 1, limit = 10 } = req.query;
-    const data = await Bat.paginate({
-      page: parseInt(page),
-      limit: parseInt(limit)
-    });
-    res.json({ bat: data });
-  } catch (e) {
-    res.status(400).json({ bat: data });
-  }
-});
+app
+  .get('/bat', middle, async (req, res) => {
+    try {
+      const { page = 1, limit = 10 } = req.query
+      const data = await Bat.paginate(
+        {},
+        {
+          page: parseInt(page),
+          limit: parseInt(limit)
+        }
+      )
+      res.json({ bat: data })
+    } catch (error) {
+      res.json({ error: error.message, type: error.name })
+    }
+  })
 
-app.get("/bat/:id", middle, async (req, res) => {
-  const { id } = req.params;
-  const data = await Bat.findOne({ _id: id });
-  res.json({ user: data });
-});
+  .get('/bat/:id', middle, async (req, res) => {
+    try {
+      const { id } = req.params
+      const data = await Bat.findOne({ _id: id })
+      res.json({ user: data })
+    } catch (error) {
+      res.json({ error: error.message, type: error.name })
+    }
+  })
 
-app.post("/bat", middle, Upload.single("file"), async (req, res) => {
-  try {
-    await Bat.create({
-      ...req.body,
-      file: { ...req.file },
-      owner: req.owner
-    });
-    res.json({ message: "arquivo cadastrado" });
-  } catch (error) {}
-});
+  .post('/bat', middle, Upload.single('file'), async (req, res) => {
+    try {
+      await Bat.create({
+        ...req.body,
+        file: { ...req.file },
+        owner: req.owner
+      })
+      res.json({ message: 'arquivo cadastrado' })
+    } catch (error) {
+      res.json({ error: error.message, type: error.name })
+    }
+  })
 
-app.post("/bat/:id/star", middle, async (req, res) => {
-  const { id } = req.params;
-  let bat = await Bat.findOne({ _id: id });
-  let data = await Bat.updateOne({ _id: id }, { star: bat.star + 1 });
-  res.json({ data });
-});
+  .patch('/bat/:id', middle, async (req, res) => {
+    try {
+      const { id } = req.params
+      let data = await Bat.updateOne({ _id: id }, req.body)
+      res.json({ data })
+    } catch (error) {
+      res.json({ error: error.message, type: error.name })
+    }
+  })
 
-app.patch("/bat/:id", middle, async (req, res) => {
-  const { id } = req.params;
-  let data = await Bat.updateOne({ _id: id }, req.body);
-  res.json({ data });
-});
+  .get('/bat/:id/download', async (req, res) => {
+    try {
+      const { id } = req.params
+      const { file } = await Bat.findOne({ _id: id })
+      res.download(
+        path.join(path.resolve('./'), '/uploads', file.filename),
+        file.originalname
+      )
+    } catch (error) {
+      res.json({ error: error.message, type: error.name })
+    }
+  })
 
-app.listen("3001", () => console.log("server listering in port 3001"));
+app.listen('3001', () => console.log('server listering in port 3001'))
