e307b16f5bd53a2f6baab3305947d1379fc2d32f
brunooomelo <utibmelo@gmail.com>
2018-11-04 14:42:32 -0200

    middleware de auth

--- bat/src/services/auth.js
+++ bat/src/services/auth.js
@@ -1,22 +1,22 @@
-const axios = require("axios");
+const axios = require('axios')
 
 module.exports = async (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization
     if (!authHeader) {
-      return res.status(401).json({ error: "No token provided" });
+      return res.status(401).json({ error: 'No token provided' })
     }
-    if (authHeader.split(" ").length !== 2) {
-      return res.status(401).json({ error: "Token error" });
+    if (authHeader.split(' ').length !== 2) {
+      return res.status(401).json({ error: 'Token error' })
     }
-    if (!/^Bearer$/i.test(authHeader.split(" ")[0])) {
-      return res.status(401).json({ error: "Token malformatted" });
+    if (!/^Bearer$/i.test(authHeader.split(' ')[0])) {
+      return res.status(401).json({ error: 'Token malformatted' })
     }
-    axios.defaults.headers.common["Authorization"] = req.headers.authorization;
-    let auth = await axios.post("http://localhost:3000/release");
-    req.owner = auth.data.user;
-    next();
+    axios.defaults.headers.common['Authorization'] = req.headers.authorization
+    let auth = await axios.post('http://localhost:3000/release')
+    req.owner = auth.data.user
+    next()
   } catch (error) {
-    return res.status(301).json({ error: "Token invalid" });
+    return res.status(301).json({ error: 'Token invalid' })
   }
-};
+}
